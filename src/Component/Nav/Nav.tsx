968cff3ff3070194d62a91d4af63f9308cec406e
dether <dether2011@gmail.com>
2023-12-24 00:59:22 -0300

    nav configure

--- src/Component/Nav/Nav.tsx
+++ src/Component/Nav/Nav.tsx
@@ -18,19 +18,19 @@ const Nav: React.FC = () => {
 		<div>
 			<nav className={`navbar ${isNavOpen ? "open" : ""}`}>
 				<div className={`nav-links ${isNavOpen ? "open" : ""}`}>
-					<NavLink to="/" className="nav-link" onClick={closeNav}>
+					<NavLink to="/Apartment-Rental" className="nav-link" onClick={closeNav}>
 						Inicio
 					</NavLink>
-					<NavLink to="/departaments" className="nav-link" onClick={closeNav}>
+					<NavLink to="/Apartment-Rental/departaments" className="nav-link" onClick={closeNav}>
 						Departamentos
 					</NavLink>
 					<div className="logo">
 						<img src={keys} alt="logo" />
 					</div>
-					<NavLink to="/services" className="nav-link" onClick={closeNav}>
+					<NavLink to="/Apartment-Rental/services" className="nav-link" onClick={closeNav}>
 						Servicios
 					</NavLink>
-					<NavLink to="/contact" className="nav-link" onClick={closeNav}>
+					<NavLink to="/Apartment-Rental/contact" className="nav-link" onClick={closeNav}>
 						Contacto
 					</NavLink>
 				</div>
